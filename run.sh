4ad05a1ceb0bca43b7bd2c498c76c97ce8c20471
Vincent Mi <miwenshu@gmail.com>
2019-04-09 00:00:11 +0800

    1

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "success <br/> vincent-ng  "`hostname` > /usr/share/nginx/html/index.html
+echo "<h1> Nginx starting</h1> <p>images from <a href=https://hub.docker.com/r/vincentmi/ng>https://hub.docker.com/r/vincentmi/ng</a> </p><p> hostname="`hostname` > /usr/share/nginx/html/index.html
 nginx -g "daemon off;"
